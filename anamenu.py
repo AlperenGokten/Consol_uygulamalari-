748c21b815d2dae660d6e0c492759567f4abfcd4
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-15 19:16:03 +0300

    Add files via upload

--- anamenu.py
+++ anamenu.py
@@ -1,5 +1,5 @@
 import takvis.takvim
-
+import hesapmakinesii.hesapmakinesi
 
 def anamenu():   
 
@@ -9,15 +9,19 @@ def anamenu():
     print("║ 3-TAKVİM          ║")
     print("║ 4-RİTMİK SAYMA    ║")
     print("║ 5-NOT HESAPLAMA   ║")
+    print("║ 6-İDEAL KİLO      ║")
+    print("║   HESAPLAMA       ║")
+    print("║ 7-ÇARPIM TABLOSU  ║")
+    print("║ 8-SINIF LİSTESİ   ║")
+    print("║ 9-HESAP MAKİNASI  ║")
+    print("║ 10-DÖVİZ UYGULAMAS║")
     print("╚═══════════════════╝")
     secim = input()
-
     if secim == "3" :
        takvis.takvim.takvimmenu()
        anamenu()
-
+    if secim == "9" :
+       python.hesapmakinesi.hesapmenu()
+       anamenu()
+ 
 anamenu()
-
-
-
-
